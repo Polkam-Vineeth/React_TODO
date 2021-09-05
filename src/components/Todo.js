35058c80afc9431e40a9a69edcd6c4abbd11548d
Polkam-Vineeth <polkamvineeth@gmail.com>
2021-09-05 22:21:30 +0530

    React Todo

---
+++ src/components/Todo.js
@@ -0,0 +1,131 @@
+import React, {useState, useEffect} from 'react'
+import "../App.css"
+
+const getLocalItmes = () => {
+    let list = localStorage.getItem('lists');
+
+    if (list) {
+        return JSON.parse(localStorage.getItem('lists'));
+    } else {
+        return [];
+    }
+}
+
+const Todo = () => {
+
+    const [inputData, setInputData] = useState('');
+    const [items, setItems] = useState(getLocalItmes());
+    const [toggleSubmit, setToggleSubmit] = useState(true);
+    const [isEditItem, setIsEditItem] = useState(null);
+    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    var day = days[ new Date().getDay()];
+    const addItem = () => {
+        if (!inputData) {
+            alert('Hey there!! Please add your Valuable Tasks to the list. 🤗');
+        } else if(inputData && !toggleSubmit) {
+            setItems(
+                items.map((elem) => {
+                    if (elem.id === isEditItem) {
+                        return { ...elem, name: inputData }
+                    }
+                    return elem;
+                })
+            )
+                 setToggleSubmit(true);
+
+                 setInputData('');
+
+                 setIsEditItem(null);
+        } else {
+            const allInputData = { id: new Date().getTime().toString(), name:inputData }
+            setItems([...items, allInputData]);
+            setInputData('')
+        }
+    }
+
+    // delete the items
+    const deleteItem = (index) => {
+        const updateditems = items.filter((elem) => {
+            return index !== elem.id;
+        });
+
+        setItems(updateditems);
+    }
+    
+    const editItem = (id) => {
+        let newEditItem = items.find((elem) => {
+            return elem.id === id
+        });
+        console.log(newEditItem);
+
+        setToggleSubmit(false);
+
+        setInputData(newEditItem.name);
+
+        setIsEditItem(id);
+
+    }
+
+    // remove all 
+    const removeAll = () => {
+         setItems([]);
+    }
+
+    // add data to localStorage
+    useEffect(() => {
+       localStorage.setItem('lists', JSON.stringify(items))
+    }, [items]);
+
+    return (
+        <>
+            <div className="main-div">
+                <div className="child-div">
+                  
+                <h1 style={{fontSize:'3.5em',fontFamily:'Satisfy'}} >Heyah , It's {day}.</h1>
+                    <figure>
+                        <img src='/todoimg.png' alt="todologo" 
+                        style={{width:'250px', height:'250px'}}/>
+                        <h2 style={{fontFamily:'Roboto'}}>Add Tasks to your TODO list here! 🎯</h2>
+                    </figure>
+                    <div className="addItems">
+                        <input type="text" placeholder="✍ Add Tasks..."
+                           value={inputData} 
+                           onChange={(e) => setInputData(e.target.value) }
+                        />
+                        {
+                            toggleSubmit ? <i className="fa fa-plus add-btn" title="Add Item" onClick={addItem}></i> :
+                                 <i className="fa fa-edit add-btn" title="Update Item" onClick={addItem}></i>
+                        }
+                       
+                    </div>
+
+                    <div className="showItems">
+                        {
+                            items.map((elem) => {
+                                return (
+                                    <div className="eachItem" key={elem.id}>
+                                        <h3>{elem.name}</h3>
+                                        <div className="todo-btn">
+                                            <i className="fa fa-edit add-btn" title="Edit Item" onClick={() => editItem(elem.id)}></i>
+                                            <i className="fa fa-trash add-btn" title="Delete Item"
+                                            onClick={() => deleteItem(elem.id)}></i>
+                                        </div>
+                                  </div>
+                                )
+                            })
+
+                        }
+                       
+                    </div>
+                
+                    {/* clear all button  */}
+                    <div className="showItems">
+                        <button className="btn effect04" data-sm-link-text="Remove All" onClick={removeAll}><span> CHECK LIST </span> </button>
+                    </div>
+                </div>
+          </div>  
+        </>
+    )
+}
+
+export default Todo
